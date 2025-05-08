74844febe0ef661b2ea0fc0f0808e7fecb0dcfcc
Eduard Sebastian Gaici <ega0021@alu.medac.es>
2025-05-08 21:03:06 +0200

    Progreso en contacto

---
+++ js/about/contacto.js
@@ -0,0 +1,21 @@
+// const formulario = document.querySelector('.contacto-formulario');
+// const modal = document.getElementById('contactoModal');
+// const inputCorreo = document.getElementById('contactoInputEmail');
+// const spanModal = document.getElementById('nombreMostrado');
+
+// formulario.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     const correo = inputCorreo.value.trim();
+//     if (!correo) return;
+
+//     spanCorreo.textContent = correo;
+//     modal.classList.add('activo');
+
+//     inputCorreo.value = ''; // vaciar el input
+
+//     // Ocultar el modal después de 3 segundos
+//     setTimeout(() => {
+//         modal.classList.remove('activo');
+//     }, 3000);
+// });
