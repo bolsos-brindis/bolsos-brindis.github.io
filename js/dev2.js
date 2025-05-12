dc07300bbed3c9ad58bc78a1ce230a0b2727f5d0
Eduard Sebastian Gaici <ega0021@alu.medac.es>
2025-05-13 00:51:07 +0200

    basura en el sobre nosotros

---
+++ js/dev2.js
@@ -0,0 +1,16 @@
+function ajustarAlturaSobreNosotros() {
+    const barra = document.getElementById("barraNavegacion");
+    const seccion = document.querySelector(".sobre-nosotros");
+
+    if (barra && seccion) {
+        const alturaNavbar = barra.offsetHeight;
+        const alturaViewport = window.innerHeight;
+        seccion.style.minHeight = (alturaViewport - alturaNavbar) + "px";
+    }
+}
+
+// Ejecutar al cargar y al redimensionar
+window.addEventListener("load", ajustarAlturaSobreNosotros);
+if (!isTouchDevice()) {
+    window.addEventListener("resize", ajustarAlturaSobreNosotros);
+}
